7924c5cb4c0254a2f34f5fde2157257c71e5cedd
Kevin Edelmann <kevin.edelmann@clio.com>
2023-11-02 15:41:27 -0700

    Changed Decree model to Note; removed table dropping on each run; changed talk from action to sense

--- lib/ruby_warrior/note.rb
+++ lib/ruby_warrior/note.rb
@@ -1,9 +1,9 @@
 require 'active_record'
 
 module RubyWarrior
-  class Decree < ActiveRecord::Base
+  class Note < ActiveRecord::Base
     establish_connection adapter: "sqlite3", database: "warriors/foobar.db"
-    connection.create_table table_name, force: true do |t|
+    connection.create_table table_name, if_not_exists: true do |t|
       t.string :body
     end
   end
