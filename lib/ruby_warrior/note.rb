c44111250545b0c68b75f9bbcc8cfc4df0205c57
Herve Grosdemanche <herve.grosdemanche@clio.com>
2023-11-03 11:01:00 -0600

    small db update

--- lib/ruby_warrior/note.rb
+++ lib/ruby_warrior/note.rb
@@ -5,7 +5,7 @@ module RubyWarrior
     establish_connection adapter: "sqlite3", database: "warriors/map_survey.db"
     connection.create_table :notes, if_not_exists: true do |t|
       t.column :level_number, :int, limit: 3, :null => false
-      t.column :type, :string, limit: 20, :null => false
+      t.column :unit_type, :string, limit: 20, :null => false
       t.column :x, :int, limit:3, :null => false
       t.column :y, :int, limit:3, :null => false
     end
