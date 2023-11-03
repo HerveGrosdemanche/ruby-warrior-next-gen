9e76ef5717c7d7621b0b0a3b3c2dde8018ad028d
Herve Grosdemanche <herve.grosdemanche@clio.com>
2023-11-02 18:48:06 -0600

    Defining up level 2

--- towers/clio/level_002.rb
+++ towers/clio/level_002.rb
@@ -1,15 +1,39 @@
-#  ---
-# |@ >|
-#  ---
+#  ------------------------
+# |@               . sCCs  |
+# |i                  CC   |
+# |                  s  s  |
+# |                        |
+# |   .                    |
+# |   .               .   >|
+# |   .                    |
+#  ------------------------
 
-description "TODO"
-tip "TODO"
+description "As you exit the castle, you see ... the great outdoors. Go on, explore, but be careful. Bandits have been setting traps around!"
+tip "Use warrior.feel.deadly? to see if there is a trap in front of you."
+clue "Make sure to check every step you take for traps."
 
 time_bonus 20 #TODO: Refine
 ace_score 19 #TODO: Refine
-size 3, 1
-stairs 2, 0
+size 24, 7
+stairs 23, 5
 
-warrior 0, 0, :east do |u|
-  u.add_abilities :walk!, :feel, :direction_of_stairs, :talk!
+warrior 0, 0, :east
+
+unit :informant, 1, 0, :east do |u|
+  u.response = "Remember warrior, the King wants you to do something for him..."
 end
+
+unit :sludge, 19, 0, :west
+unit :sludge, 22, 0, :east
+unit :sludge, 19, 2, :south
+unit :sludge, 22, 2, :south
+unit :captive, 20, 0, :west
+unit :captive, 21, 0, :east
+unit :captive, 20, 1, :west
+unit :captive, 21, 1, :east
+
+unit :trap, 17, 0
+unit :trap, 20, 5
+unit :trap, 3, 4
+unit :trap, 3, 5
+unit :trap, 3, 6
