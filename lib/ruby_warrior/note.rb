d7509906385b4f721fd0f11f736972db8ad82d00
Herve Grosdemanche <herve.grosdemanche@clio.com>
2023-11-03 10:49:57 -0600

    rename the table

--- lib/ruby_warrior/note.rb
+++ lib/ruby_warrior/note.rb
@@ -3,7 +3,7 @@ require 'active_record'
 module RubyWarrior
   class Note < ActiveRecord::Base
     establish_connection adapter: "sqlite3", database: "warriors/map_survey.db"
-    connection.create_table :map_items, if_not_exists: true do |t|
+    connection.create_table :notes, if_not_exists: true do |t|
       t.column :level_number, :int, limit: 3, :null => false
       t.column :type, :string, limit: 20, :null => false
       t.column :x, :int, limit:3, :null => false
