e68ce197ed9121889f3e146598955f7d8d9975e7
Herve Grosdemanche <herve.grosdemanche@clio.com>
2023-11-03 12:51:05 -0600

    adding the ActiveRecord validation logic

--- lib/ruby_warrior/note.rb
+++ lib/ruby_warrior/note.rb
@@ -2,7 +2,7 @@ require 'active_record'
 
 module RubyWarrior
   class Note < ActiveRecord::Base
-    enum unit_type: ["archer", "bug", "captive" "guard", "informant", "queen_bug", "sludge", "thick_sludge", "trap"]
+    enum unit_type: ["archer", "bug", "captive", "guard", "informant", "queen_bug", "sludge", "thick_sludge", "trap"]
     establish_connection adapter: "sqlite3", database: "warriors/map_survey.db"
     connection.create_table :notes, if_not_exists: true do |t|
       t.column :level_number, :int, limit: 3, :null => false
--- lib/ruby_warrior/units/guard.rb
+++ lib/ruby_warrior/units/guard.rb
@@ -10,7 +10,15 @@ module RubyWarrior
         [:forward, :left, :right, :backward].each do |direction|
           if turn.feel(direction).player?
             # TODO: Add the ActiveRecord check here
-
+            grade = check_notes
+            passing_grade = 0.75
+            if grade >= passing_grade
+              say success
+              @position = nil
+            else
+              say "Your grade was #{grade.round(2)}, but you needed #{passing_grade} to pass."
+              say failure
+            end
             # This should happen only if the ActiveRecord check fails, and the guard's health goes down by half
             turn.attack!(direction) if health < max_health / 2
             return
@@ -31,6 +39,65 @@ module RubyWarrior
 
       def character
         "g"
+      end
+
+      def check_notes
+        expected_notes = [
+          RubyWarrior::Note.new(level_number: 1, unit_type: "informant", x: 8, y: 1),
+          RubyWarrior::Note.new(level_number: 1, unit_type: "informant", x: 5, y: 4),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "sludge", x: 19, y: 0),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "sludge", x: 22, y: 0),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "sludge", x: 19, y: 2),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "sludge", x: 22, y: 2),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "captive", x: 20, y: 0),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "captive", x: 21, y: 0),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "captive", x: 20, y: 1),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "captive", x: 21, y: 1),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "trap", x: 17, y: 0),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "trap", x: 20, y: 5),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "trap", x: 3, y: 4),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "trap", x: 3, y: 5),
+          RubyWarrior::Note.new(level_number: 2, unit_type: "trap", x: 3, y: 6),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 0, y: 0),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 19, y: 0),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 4, y: 1),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 20, y: 3),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 12, y: 4),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "bug", x: 10, y: 5),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "queen_bug", x: 22, y: 6),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "trap", x: 11, y: 2),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "trap", x: 11, y: 3),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "trap", x: 11, y: 4),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "trap", x: 11, y: 5),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "trap", x: 23, y: 5),
+          RubyWarrior::Note.new(level_number: 3, unit_type: "informant", x: 0, y: 6),
+          RubyWarrior::Note.new(level_number: 4, unit_type: "trap", x: 4, y: 0),
+          RubyWarrior::Note.new(level_number: 4, unit_type: "trap", x: 4, y: 2),
+          RubyWarrior::Note.new(level_number: 4, unit_type: "guard", x: 4, y: 1),
+        ]
+
+        player_notes = RubyWarrior::Note.all
+
+        # Weird math to handle when the player has more notes than expected
+        grade = 1.0
+        if player_notes.length > expected_notes.length
+          grade = 0.8 # if we get more notes than expected, we'll just set it to 0.8, because we're not that mean but it matters that we're getting too much info
+          grading_factor = 1 / expected_notes.length.to_f
+        else
+          grading_factor = 1 / player_notes.length.to_f
+        end
+
+        expected_notes.each do |note|
+          match = false
+          player_notes.each do |player_note|
+            if player_note.level_number == note.level_number && player_note.unit_type == note.unit_type && player_note.x == note.x && player_note.y == note.y
+              match = true
+            end
+          end
+          grade = grade - grading_factor unless match
+        end
+
+        grade
       end
     end
   end
